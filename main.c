9b7dba1c3e4982ab3d9373bac0ac48b60a2d5627
JackHasaKeyboard <jackhasakeyboard@gmail.com>
2023-12-03 12:50:11 -0700

    Output song and artist name

--- main.c
+++ main.c
@@ -1,3 +1,30 @@
+#include <stdio.h>
+
+const char ws = ' ';
+
+const char sep = '-';
+
+void err(char* msg) {
+	fprintf(stderr, "Error: %s\n", msg);
+}
+
 int main(int argc, char* argv[]) {
+	if (argc == 1) {
+		err("No arguments");
+
+		return 1;
+	}
+
+	if (argc != 1 + 1 + 1) {
+		err("Wrong number of arguments");
+
+		return 1;
+	}
+
+	char* artist = argv[1];
+	char* song = argv[2];
+
+	printf("%s%c%c%c%s", artist, ws, sep, ws, song);
+
 	return 0;
 }
